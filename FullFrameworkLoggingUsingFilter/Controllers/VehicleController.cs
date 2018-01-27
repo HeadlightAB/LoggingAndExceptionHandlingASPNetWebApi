895b1c8cb1fb9287dbf796a7d872a7512a7da683
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2018-01-27 10:14:44 +0100

    Prepared for logging, e.g. controller and action filter

---
+++ FullFrameworkLoggingUsingFilter/Controllers/VehicleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Web.Http;
+
+namespace FullFrameworkLoggingUsingFilter.Controllers
+{
+    [RoutePrefix("api/vehicles")]
+    public class VehicleController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetAll()
+        {
+            return Ok(new {a = 123});
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult Post()
+        {
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ FullFrameworkLoggingUsingFilter/LoggingActionFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace FullFrameworkLoggingUsingFilter
+{
+    public class LoggingActionFilter : ActionFilterAttribute
+    {
+        private const string XCorrelationHeader = "x-correlation";
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            try
+            {
+                // Logging goes here
+                var method = actionContext.Request.Method;
+                var user = actionContext.RequestContext.Principal.Identity;
+                var url = actionContext.Request.RequestUri;
+                actionContext.Request.Headers.Add(XCorrelationHeader, Guid.NewGuid().ToString());
+            }
+            catch
+            {
+                // ignored, logging should not make the api die
+            }
+
+            base.OnActionExecuting(actionContext);
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            try
+            {
+                // Logging goes here
+                var statusCode = actionExecutedContext.Response.StatusCode;
+                var correlationId = actionExecutedContext.Request.Headers.GetValues(XCorrelationHeader).Single();
+            }
+            catch
+            {
+                // ignored, logging should not make the api die
+            }
+
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
